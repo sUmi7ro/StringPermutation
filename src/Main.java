5f4debbc2ec416df5add2fbef80e062f8d534fe9
sUmi7ro <simonto.no@outlook.com>
2024-09-10 00:27:24 +0200

    Start...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "ABCD";
+
+        permute(str.toCharArray(), 0);
+    }
+
+    public static void permute(char[] seq, int pos){
+        for (int i = 0; i < seq.length; i++) {
+            bytt(seq, seq[pos], seq[pos+1]);
+            permute(seq, pos+1);
+            bytt();
+        }
+
+    }
+
+    public static void bytt(char[] seq, char c1, char c2){
+
     }
 }
