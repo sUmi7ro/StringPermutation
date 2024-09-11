a6b2b35a7f0c8da9e0aa06d42d48f97ab05f267b
sUmi7ro <simonto.no@outlook.com>
2024-09-11 10:54:52 +0200

    Start....

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String str = "ABCD";
+        String str = "ABC";
 
         permute(str.toCharArray(), 0);
     }
 
     public static void permute(char[] seq, int pos){
         for (int i = 0; i < seq.length; i++) {
-            bytt(seq, seq[pos], seq[pos+1]);
+
+            System.out.println(Arrays.toString(seq));
+
+            int pos1 = pos+1;
+            int pos2 = pos1+1;
+
+            bytt(seq, pos1, pos2);
             permute(seq, pos+1);
             bytt();
         }
 
     }
 
-    public static void bytt(char[] seq, char c1, char c2){
-
+    public static void bytt(char[] seq, int pos1, int pos2){
+        char temp = seq[pos1];
+        seq[pos1] = seq[pos2];
+        seq[pos2] = temp;
     }
 }
