bb3ae808c8c40f90a71f874f3693b196c88ce5b3
sUmi7ro <simonto.no@outlook.com>
2024-09-15 22:32:38 +0200

    Only works with "ABC"

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,33 @@ public class Main {
         String str = "ABC";
 
         permute(str.toCharArray(), 0);
+        //System.out.println(str);
     }
 
     public static void permute(char[] seq, int pos){
-        for (int i = 0; i < seq.length; i++) {
+        System.out.println(pos);
+        for (int i=0;i < seq.length-1 && pos < seq.length-1;i++) {
+            //if(pos >= seq.length) return;
+            int pos1 = i*2;
 
-            System.out.println(Arrays.toString(seq));
+            //System.out.println("i="+i);
+            int pos2 = pos;
 
-            int pos1 = pos+1;
-            int pos2 = pos1+1;
 
             bytt(seq, pos1, pos2);
+            System.out.println(Arrays.toString(seq));
+            //System.out.println("p="+pos);
             permute(seq, pos+1);
-            bytt();
+            bytt(seq, pos2, pos1);
         }
 
+        /*
+        * Bytt en og la neste gjøre resten.
+        * */
     }
 
     public static void bytt(char[] seq, int pos1, int pos2){
+        //System.out.println(pos1+" "+pos2);
         char temp = seq[pos1];
         seq[pos1] = seq[pos2];
         seq[pos2] = temp;
